54b33f40c87e980b822b94237a09a9dd8c179e50
David Miotti <david.miotti@wopata.com>
2017-06-12 11:44:36 +0200

    Use staging firebase database

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase'
 
+// const config = {
+  // apiKey: 'AIzaSyC1H6htvMyLm0xBTGFBHaqgwAnohhclFaI',
+  // authDomain: 'wopafoot.firebaseapp.com',
+  // databaseURL: 'https://wopafoot.firebaseio.com',
+// }
+
 const config = {
-  apiKey: 'AIzaSyC1H6htvMyLm0xBTGFBHaqgwAnohhclFaI',
-  authDomain: 'wopafoot.firebaseapp.com',
-  databaseURL: 'https://wopafoot.firebaseio.com',
+  apiKey: 'AIzaSyD-fLr00jQBO9iUx7SYOEKfMxq3-d2zzxo',
+  authDomain: 'wopafoot-staging.firebaseapp.com',
+  databaseURL: 'https://wopafoot-staging.firebaseio.com',
 }
 
 firebase.initializeApp(config)
