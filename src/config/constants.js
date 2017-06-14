d3f824e17c7404d8805dcdcebc2395e84e20ebdd
David Miotti <david.miotti@wopata.com>
2017-06-14 09:59:31 +0200

    Use staging when developping

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase'
 
+const config = {
+  apiKey: 'AIzaSyD-fLr00jQBO9iUx7SYOEKfMxq3-d2zzxo',
+  authDomain: 'wopafoot-staging.firebaseapp.com',
+  databaseURL: 'https://wopafoot-staging.firebaseio.com',
+}
+
 // const config = {
 //   apiKey: 'AIzaSyC1H6htvMyLm0xBTGFBHaqgwAnohhclFaI',
 //   authDomain: 'wopafoot.firebaseapp.com',
 //   databaseURL: 'https://wopafoot.firebaseio.com',
 // }
-
-const config = {
-  apiKey: 'AIzaSyC1H6htvMyLm0xBTGFBHaqgwAnohhclFaI',
-  authDomain: 'wopafoot.firebaseapp.com',
-  databaseURL: 'https://wopafoot.firebaseio.com',
-}
 
 firebase.initializeApp(config)
 
